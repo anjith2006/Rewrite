62409604f5bbb27b0e876ca6d982737d2d2dba97
ageorge <anjith.george@idiap.ch>
2020-10-01 17:16:52 +0200

    Moda

--- rewrite.py
+++ rewrite.py
@@ -1,23 +1,24 @@
 import streamlit as st
 
-from translate_utils import forward_backward
+from translate_utils import forward_backward, LANGUAGES, LANGCODES, HTML_WRAPPER
 
 language= st.sidebar.selectbox(
-    'Select the language to Forward back ward',['French:fr','German:de','Hindi:hi'])
+	'Select the language to forward-backward',['german','french']+list(LANGCODES.keys()))
+
 
 
 times= st.sidebar.selectbox(
     'Number of times',[1,2,3,4,5])
 
-# print("language: {}, times: {}".format(language,times))
-
 st.title('Rewrite:')
-#text = st.text_input('The sentence to rewrite.')
 
 text = st.text_area('The sentence to rewrite.')
 
 agree = st.button('Rewrite!')
 
 if agree:
-	trans=forward_backward(text,lang=language.split(":")[-1],times=int(times))
-	st.write(trans)
+	trans=forward_backward(text,lang=LANGCODES[language],times=int(times)) 
+	st.write(HTML_WRAPPER.format(trans),unsafe_allow_html=True)
+
+
+	
